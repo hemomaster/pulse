f4d3fb84842c32bcaf7c0fc99059844916852b32
hemomaster <hemomaster@dmail.com>
2021-05-16 15:34:16 +0300

    Сверстал, стилизовал и адаптировал секцию Carousel

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,25 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   const telFields = document.querySelectorAll("input[type='tel']");
+  const carousel = document.querySelector(".carousel__container");
   const forms = document.querySelectorAll("form");
+
+  const swiper = new Swiper(carousel, {
+    slidesPerView: 1,
+    // spaceBetween: 50,
+    loop: true,
+    navigation: {
+      nextEl: ".carousel__btn-next",
+      prevEl: ".carousel__btn-prev",
+    },
+  });
 
   // INPUTMASK
   Inputmask("+380 (99) 999-99-99").mask(telFields);
 
   // JUSTVALIDATE
-  const validateForm = (formEl, rules) => {
-    new JustValidate(formEl, {
+  const validateForm = (selector, rules) => {
+    new JustValidate(selector, {
       rules,
       submitHandler: function (form) {
         const dataForm = new FormData(form);
